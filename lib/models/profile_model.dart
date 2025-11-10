a1316a0189b6d521dada3ebbbadeba28c12c239b
Susan Jong <susanjong@students.usu.ac.id>
2025-11-11 02:41:19 +0700

    <feat>: add profile models

---
+++ lib/models/profile_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+// Models
+class UserProfile {
+  final String name;
+  final String email;
+  final String bio;
+  final String imageUrl;
+
+  UserProfile({
+    required this.name,
+    required this.email,
+    required this.bio,
+    required this.imageUrl,
+  });
+}
+
+class SettingItem {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final Color? iconColor;
+  final Color? iconBackgroundColor;
+  final VoidCallback? onTap;
+  final Widget? trailing;
+
+  SettingItem({
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    this.iconColor,
+    this.iconBackgroundColor,
+    this.onTap,
+    this.trailing,
+  });
+}
