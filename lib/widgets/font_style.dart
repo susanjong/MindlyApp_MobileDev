b0e300ea67ea9a5d78cd9f3c7ecc57f8d5cacc7e
Susan Jong <susanjong@students.usu.ac.id>
2025-11-13 13:21:45 +0700

    <feat>signup: refactor Create Account button and add Poppins font

--- lib/widgets/font_style.dart
+++ lib/widgets/font_style.dart
@@ -1,31 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class FontStyles {
-  // Style di judul utama
+  // Base Poppins TextTheme
+  static final TextTheme poppinsTextTheme = GoogleFonts.poppinsTextTheme();
 
-  //pakai static supaya bisa diakses langsung tanpa instance
-  static const TextStyle appTitle = TextStyle(
-    color: Color(0xFF0D5F5F),
+  // Style judul utama
+  static TextStyle appTitle = GoogleFonts.poppins(
+    color: const Color(0xFF0D5F5F),
     fontSize: 24,
     fontWeight: FontWeight.bold,
   );
 
-// TextStyle mainHeader = TextStyle(
-//   fontSize: 26,
-//   color: primaryColor
-//   fontFamily: 'Poppins',
-//   fontWeight: FontWeight.w700,
-// );
-//
-// TextStyle subHeader = TextStyle(
-//   fontSize: 20,
-//   color: Colors.green,
-//   fontFamily: 'Poppins',
-// );
-//
-// TextStyle bodyText = TextStyle(
-//   fontSize: 14,
-//   color: Colors.black,
-//   fontFamily: 'Poppins',
-// );
+  // Main header
+  static TextStyle mainHeader = GoogleFonts.poppins(
+    fontSize: 26,
+    color: const Color(0xFF0D5F5F),
+    fontWeight: FontWeight.w700,
+  );
+
+  // Sub header
+  static TextStyle subHeader = GoogleFonts.poppins(
+    fontSize: 20,
+    color: Colors.green,
+    fontWeight: FontWeight.w600,
+  );
+
+  // Body text
+  static TextStyle bodyText = GoogleFonts.poppins(
+    fontSize: 14,
+    color: Colors.black,
+    fontWeight: FontWeight.normal,
+  );
 }
