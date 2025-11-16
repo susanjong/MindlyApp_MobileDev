66ab969cb13b4e7cacfaedabc711051e8211fa16
Susan Jong <susanjong@students.usu.ac.id>
2025-11-16 13:35:11 +0700

    <refactor>: feat extract loading overlay and improve signup screen

---
+++ lib/widgets/loading_overlay.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoadingOverlay extends StatelessWidget {
+  final String message;
+
+  const LoadingOverlay({
+    super.key,
+    this.message = 'Please Wait...',
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.black.withValues(alpha: 0.5),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              message,
+              style: GoogleFonts.poppins(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
