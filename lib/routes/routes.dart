72f477045ea1327568cdfe3471de9dc9357602f1
Susan Jong <susanjong@students.usu.ac.id>
2025-11-19 09:32:24 +0700

    Refactor: relocate and redesign password reset flow

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:notesapp/presentation/screen/entry/intro_app.dart';
 import 'package:notesapp/presentation/screen/main_home/help_faq.dart';
 import 'package:notesapp/presentation/screen/entry/splash_screen.dart';
 import 'package:notesapp/presentation/screen/entry/login.dart';
 import 'package:notesapp/presentation/screen/entry/resetpass.dart';
 import 'package:notesapp/presentation/screen/entry/signup.dart';
-import 'package:notesapp/presentation/screen//entry/forgotpass.dart';
+import 'package:notesapp/presentation/screen/entry/forgotpass.dart';
 import 'package:notesapp/presentation/screen/main_home/home.dart';
 import 'package:notesapp/presentation/screen/main_home/profile.dart';
 import 'package:notesapp/presentation/screen/main_home/about_info.dart';
@@ -13,8 +14,10 @@ import 'package:notesapp/presentation/screen/todolist/awaltodo.dart';
 import 'package:notesapp/presentation/screen/calendar/awalcalendar.dart';
 
 class AppRoutes {
-  // make ur route name
+  AppRoutes._();
+
   static const String splash = '/';
+  static const String introApp = '/intro_app';
   static const String signUp = '/sign_up';
   static const String signIn = '/sign_in';
   static const String home = '/home';
@@ -27,11 +30,13 @@ class AppRoutes {
   static const String helpFaq = '/help_faq';
   static const String aboutUs = '/about_info';
 
-  // generator for route
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case splash:
-        return MaterialPageRoute(builder: (_) => const LogoSplash());
+        return MaterialPageRoute(builder: (_) => LogoSplash());
+
+      case introApp:
+        return MaterialPageRoute(builder: (_) => WelcomeScreen());
 
       case signUp:
         return MaterialPageRoute(builder: (_) => SignUpScreen());
@@ -40,7 +45,7 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => LoginAccountScreen());
 
       case home:
-        return MaterialPageRoute(builder: (_) => const HomePage());
+        return MaterialPageRoute(builder: (_) => HomePage());
 
       case resetPassword:
         return MaterialPageRoute(builder: (_) => ResetPasswordScreen());
@@ -49,22 +54,22 @@ class AppRoutes {
         return MaterialPageRoute(builder: (_) => ForgotPasswordScreen());
 
       case profile:
-        return MaterialPageRoute(builder: (_) => const AccountProfilePage());
+        return MaterialPageRoute(builder: (_) => AccountProfilePage());
 
       case notes:
-        return MaterialPageRoute(builder: (_) => const NotesPage());
+        return MaterialPageRoute(builder: (_) => NotesPage());
 
       case todo:
-        return MaterialPageRoute(builder: (_) => const TodoListPage());
+        return MaterialPageRoute(builder: (_) => TodoListPage());
 
       case calendar:
-        return MaterialPageRoute(builder: (_) => const CalendarPage());
+        return MaterialPageRoute(builder: (_) => CalendarPage());
 
       case helpFaq:
-        return MaterialPageRoute(builder: (_) => const HelpFaqScreen());
+        return MaterialPageRoute(builder: (_) => HelpFaqScreen());
 
       case aboutUs:
-        return MaterialPageRoute(builder: (_) => const AboutPage());
+        return MaterialPageRoute(builder: (_) => AboutPage());
 
       default:
         return MaterialPageRoute(
@@ -84,21 +89,21 @@ class AppRoutes {
     }
   }
 
-  // route map
   static Map<String, WidgetBuilder> get routes {
     return {
-      splash: (context) => const LogoSplash(),
+      splash: (context) => LogoSplash(),
+      introApp: (context) => WelcomeScreen(),
       signUp: (context) => SignUpScreen(),
       signIn: (context) => LoginAccountScreen(),
-      home: (context) => const HomePage(),
+      home: (context) => HomePage(),
       resetPassword: (context) => ResetPasswordScreen(),
       forgotPassword: (context) => ForgotPasswordScreen(),
-      profile: (context) => const AccountProfilePage(),
-      notes: (context) => const NotesPage(),
-      todo: (context) => const TodoListPage(),
-      calendar: (context) => const CalendarPage(),
-      helpFaq: (context) => const HelpFaqScreen(),
-      aboutUs: (context) => const AboutPage(),
+      profile: (context) => AccountProfilePage(),
+      notes: (context) => NotesPage(),
+      todo: (context) => TodoListPage(),
+      calendar: (context) => CalendarPage(),
+      helpFaq: (context) => HelpFaqScreen(),
+      aboutUs: (context) => AboutPage(),
     };
   }
 }
