095c7d26ead435f4311620b6c613a21e2a9d582b
Susan Jong <susanjong@students.usu.ac.id>
2025-12-03 14:38:29 +0700

    <feat>:implement firebase integration and task management in todo feature

--- lib/features/calendar/presentation/pages/calendar_main_page.dart
+++ lib/features/calendar/presentation/pages/calendar_main_page.dart
@@ -351,7 +351,7 @@ class _CalendarMainPageState extends State<CalendarMainPage> {
     );
   }
 
-  // --- TIMELINE BUILDER ---
+  // timeline builder
   Widget _buildScheduleTimeline() {
     return SizedBox(
       height: 320,
